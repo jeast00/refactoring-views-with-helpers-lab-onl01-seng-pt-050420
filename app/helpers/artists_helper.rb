2dd9db2c89092c8f6cd8fd19e6426bd649f4e870
Jonathan East <jonathan_east@ymail.com>
2020-09-29 13:43:10 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
     if song.artist && song.artist.name != ""
       link_to song.artist.name, artist_path(song.artist)
     else
-      link_to "Add an Artist", edit_song_path(song)
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
